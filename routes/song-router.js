1e3cab507aaf69d6586fec5c26b7093772ae4cef
JC Lee <jcleefw@hotmail.com>
2019-09-27 09:28:15 +1000

    change delete song route to be more explicit

--- routes/song-router.js
+++ routes/song-router.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/song', SongCtrl.createSong)
 router.put('/song/:id', SongCtrl.updateSong)
-router.delete('/song/:id', SongCtrl.deleteSong)
+router.delete('/delete-song/:id', SongCtrl.deleteSong)
 router.get('/song/:id', SongCtrl.getSongById)
 router.get('/songs', SongCtrl.getSongs)
 
